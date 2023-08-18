118b9bd3cc66be4cc2cb5b6cf4cf42cd6784d85b
scorpio <muhammedlameesmv@gmail.com>
2023-08-18 18:13:12 +0530

    up

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,7 @@
-pip install -r requirements.txt   
+cd..
+virtualenv venv
+. venv/bin/activate
+pip install -r requirements.txt
 python manage.py collectstatic
+python manage.py migrate
+python manage.py runserver
